71ba36e0d320f00cb0733fb6cf169c8ac84f824f
ricardodesouza1 <ricardo270296souza@gmail.com>
2024-10-10 10:09:35 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'manager-task-app-ionic',
+  webDir: 'www'
+};
+
+export default config;
