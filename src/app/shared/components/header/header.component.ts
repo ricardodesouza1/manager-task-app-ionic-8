740b1631271dc7ad7380e17f75b01ed497be1ddf
ricardodesouza1 <ricardo270296souza@gmail.com>
2024-10-10 10:37:59 -0300

    Create struct components

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent  implements OnInit {
+
+  @Input() title: string;
+  @Input() backButton: string;
+  @Input() inModal: boolean;
+  @Input() color: string;
+  @Input() centerTitle: boolean;
+  
+  constructor() { }
+
+  ngOnInit() {}
+
+}
